224678fe87f55e6275bb089715e27711021626e8
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-06-10 10:05:24 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mobobob
+
+In memory of John Hunter, who I met for the first time at SciPy 2012, and
+passed recently after. I recently learned that his children we calling
+["mobobob"](https://twitter.com/fperez_org/status/741262612977979392).
+
+
+Just an alias to [matplotlib](//matplotlib.org). 
--- mobobob/__init__.py
+++ mobobob/__init__.py
@@ -1,9 +1,12 @@
+"""
+An alias for matplotlib
+
+In memory of John Hunter, who I met for the first time at SciPy 2012, and
+passed recently after. I recently learned that his children were calling
+"mobmobmob"
 
 """
-a simple package
-"""
-
-
-__version__ = '0.0.1'
+from matplotlib import *
+__version__ = '0.5.0'
 
         
